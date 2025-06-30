93ed57d6a965b65f03e0760f51bd7ae745ce0483
Francky Razakamanantsoa <francky.razakamanantsoa@pulse.mg>
2025-06-30 10:08:21 +0300

    Socle : setting up Entity User & trait

---
+++ src/Trait/CreatedTimeTrackableTrait.php
@@ -0,0 +1,35 @@
+<?php
+namespace App\Traits;
+
+use Doctrine\ORM\Mapping as ORM;
+
+trait CreatedTimeTrackableTrait
+{
+    #[ORM\Column(type: 'datetime_immutable')]
+    private \DateTimeImmutable $createdAt;
+
+    #[ORM\JoinColumn(nullable: true, onDelete: 'SET NULL')]
+    private ?object $createdBy = null;
+
+    public function getCreatedAt(): \DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+        return $this;
+    }
+
+    public function getCreatedBy(): ?object
+    {
+        return $this->createdBy;
+    }
+
+    public function setCreatedBy(?object $createdBy): self
+    {
+        $this->createdBy = $createdBy;
+        return $this;
+    }
+}
