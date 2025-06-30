6ed2f85f8adf88f70c44b7eebdd68f2aee794776
Francky Razakamanantsoa <francky.razakamanantsoa@pulse.mg>
2025-06-30 14:39:36 +0300

    Socle : Maj entity User for authentication

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -1,13 +1,13 @@
 <?php
-
 namespace App\Entity;
 
 use App\Repository\UserRepository;
 use App\Trait\CreatedTimeTrackableTrait;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-class User
+class User implements UserInterface
 {
     use CreatedTimeTrackableTrait;
 
@@ -27,6 +27,9 @@ class User
 
     #[ORM\Column]
     private ?bool $status = null;
+
+    #[ORM\Column(nullable: true)]
+    private array $roles = [];
 
     public function getId(): ?int
     {
@@ -79,5 +82,46 @@ class User
         $this->status = $status;
 
         return $this;
+    }
+
+    /**
+     * @see UserInterface
+     *
+     * @return list<string>
+     */
+    public function getRoles(): array
+    {
+        $rolesUser = $this->roles;
+        // guarantee every user at least has ROLE_USER : $rolesUser[] = 'ROLE_USER';
+        return array_unique($rolesUser);
+    }
+
+    /**
+     * @param list<string> $roles
+     */
+    public function setRoles(array $roles): static
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials(): void
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
     }
 }
