181c4b0564a258ea1ada592dff52c99629a38067
Francky Razakamanantsoa <francky.razakamanantsoa@pulse.mg>
2025-06-30 10:13:29 +0300

    Socle : Create TimeTrackableTrait

---
+++ src/Trait/CreatedOnlyTimeTrackableTrait.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Traits;
+
+use Doctrine\ORM\Mapping as ORM;
+
+trait CreatedOnlyTimeTrackableTrait
+{
+    #[ORM\Column(type: 'datetime_immutable', nullable: true)]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    public function getCreatedAt() : \DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+        return $this;
+    }
+}
