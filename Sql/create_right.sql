f8f4f5175f9ab371aa7b98aa689a2a615aea0e00
Francky Razakamanantsoa <francky.razakamanantsoa@pulse.mg>
2025-07-02 16:33:53 +0300

    Role : Script SQL to populate right

--- Sql/create_right.sql
+++ Sql/create_right.sql
@@ -1,6 +1,28 @@
--- Administrateur
+-- Company
 	INSERT INTO `right` (`code`, `label`)
-	VALUES ('READ_AGENT', 'Lecture Agent'),
-        ('IMPORT_AGENT', 'Import Agent'),
-		('EXPORT_AGENT', 'Export Agent'),
-		('DOWN_IMPORT_MODEL_AGENT', 'Téléchargement modèle fichier import Agent');
+	VALUES ('CREATE_COMPANY', 'Création société'),
+        ('EDIT_COMPANY', 'Modification société'),
+		('READ_COMPANY', 'Lecture société'),
+		('STATUS_COMPANY', 'Activer/Desactiver société');
+
+-- User
+	INSERT INTO `right` (`code`, `label`)
+	VALUES ('CREATE_USER', 'Création utilisateur'),
+        ('EDIT_USER', 'Modification utilisateur'),
+		('READ_USER', 'Lecture utilisateur'),
+		('STATUS_USER', 'Activer/Desactiver utilisateur');
+
+-- Msisdn Fleet
+	INSERT INTO `right` (`code`, `label`)
+	VALUES ('CREATE_MSISDNFLEET', 'Création ligne flotte (MSISDN)'),
+        ('EDIT_MSISDNFLEET', 'Modification ligne flotte (MSISDN)'),
+		('READ_MSISDNFLEET', 'Lecture ligne flotte (MSISDN)'),
+		('REPORT_MSISDNFLEET', 'Génération rapports');
+
+-- Billing
+	INSERT INTO `right` (`code`, `label`)
+	VALUES 
+		('CREATE_BILLING', 'Création facture'),
+		('READ_BILLING', 'Lecture facture'),
+		('EDIT_BILLING', 'Modification facture'),
+		('DOWN_BILLING', 'Télécharger facture');
