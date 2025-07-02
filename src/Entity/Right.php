9a29b1bedebe45fc788f07717590973334c20abf
Francky Razakamanantsoa <francky.razakamanantsoa@pulse.mg>
2025-07-02 13:59:10 +0300

    Role : Set property code and label on right table

--- src/Entity/Right.php
+++ src/Entity/Right.php
@@ -2,35 +2,23 @@
 
 namespace App\Entity;
 
-use App\Repository\RightRepository;
+use App\Trait\CodeLabelTrait;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\RightRepository;
 
 #[ORM\Entity(repositoryClass: RightRepository::class)]
 #[ORM\Table(name: '`right`')]
 class Right
 {
+    use CodeLabelTrait;
+    
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $description = null;
-
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function getDescription(): ?string
-    {
-        return $this->description;
-    }
-
-    public function setDescription(string $description): static
-    {
-        $this->description = $description;
-
-        return $this;
     }
 }
