636934b51a150377ef8554698266a6485cf25781
Francky Razakamanantsoa <francky.razakamanantsoa@pulse.mg>
2025-07-02 14:25:44 +0300

    Role : Sql to populate right table

---
+++ Sql/create_right.sql
@@ -0,0 +1,6 @@
+-- Administrateur
+	INSERT INTO `right` (`code`, `label`)
+	VALUES ('READ_AGENT', 'Lecture Agent'),
+        ('IMPORT_AGENT', 'Import Agent'),
+		('EXPORT_AGENT', 'Export Agent'),
+		('DOWN_IMPORT_MODEL_AGENT', 'Téléchargement modèle fichier import Agent');
