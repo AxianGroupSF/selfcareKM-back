d2d4dbbb44521450b8ce664b5268e243411b3517
Francky Razakamanantsoa <francky.razakamanantsoa@pulse.mg>
2025-07-02 14:05:09 +0300

    Role : Relation between Role and Right

--- src/Entity/Right.php
+++ src/Entity/Right.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Trait\CodeLabelTrait;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use App\Repository\RightRepository;
 
@@ -17,8 +19,46 @@ class Right
     #[ORM\Column]
     private ?int $id = null;
 
+    /**
+     * @var Collection<int, Role>
+     */
+    #[ORM\ManyToMany(targetEntity: Role::class, mappedBy: 'rights')]
+    private Collection $roles;
+
+    public function __construct()
+    {
+        $this->roles = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    /**
+     * @return Collection<int, Role>
+     */
+    public function getRoles(): Collection
+    {
+        return $this->roles;
+    }
+
+    public function addRole(Role $role): static
+    {
+        if (!$this->roles->contains($role)) {
+            $this->roles->add($role);
+            $role->addRight($this);
+        }
+
+        return $this;
+    }
+
+    public function removeRole(Role $role): static
+    {
+        if ($this->roles->removeElement($role)) {
+            $role->removeRight($this);
+        }
+
+        return $this;
     }
 }
--- src/Entity/Role.php
+++ src/Entity/Role.php
@@ -6,6 +6,8 @@ use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
 use App\Repository\RoleRepository;
 use App\Trait\CodeLabelTrait;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
@@ -29,8 +31,43 @@ class Role
     #[ORM\Column]
     private ?int $id = null;
 
+    /**
+     * @var Collection<int, right>
+     */
+    #[ORM\ManyToMany(targetEntity: right::class, inversedBy: 'roles')]
+    private Collection $rights;
+
+    public function __construct()
+    {
+        $this->rights = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    /**
+     * @return Collection<int, right>
+     */
+    public function getRights(): Collection
+    {
+        return $this->rights;
+    }
+
+    public function addRight(right $right): static
+    {
+        if (!$this->rights->contains($right)) {
+            $this->rights->add($right);
+        }
+
+        return $this;
+    }
+
+    public function removeRight(right $right): static
+    {
+        $this->rights->removeElement($right);
+
+        return $this;
     }
 }
