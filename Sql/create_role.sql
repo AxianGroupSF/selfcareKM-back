3d3edeb7a4b33d997119fc72604f76d004d9dec2
Francky Razakamanantsoa <francky.razakamanantsoa@pulse.mg>
2025-07-02 14:13:52 +0300

    Role : Sql to populate Role table

---
+++ Sql/create_role.sql
@@ -0,0 +1,17 @@
+INSERT INTO
+    `role` (
+        `code`,
+        `label`
+    )
+VALUES (
+        'ROLE_ADMIN',
+        'Administrateur'
+    ),
+    (
+        'ROLE_MANAGER',
+        'It Manager'
+    ),
+    (
+        'ROLE_BACKOFFICE',
+        'Backoffice'
+    );
