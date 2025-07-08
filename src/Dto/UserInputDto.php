407ae0f6ef9f0363f982977fa49cdcd1ebc27b9a
Francky Razakamanantsoa <francky.razakamanantsoa@pulse.mg>
2025-07-08 11:18:28 +0300

    Role & User : Schema for create user

--- src/Dto/UserInputDto.php
+++ src/Dto/UserInputDto.php
@@ -2,30 +2,45 @@
 namespace App\Dto;
 
 use ApiPlatform\Metadata\ApiProperty;
+use ApiPlatform\Metadata\ApiResource;
+use App\Constante\SelfcareConst;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 
+#[ApiResource(
+    shortName: 'UserInput',
+    description: 'Payload for creating a user',
+    deprecated: false,
+    denormalizationContext: ['groups' => [SelfcareConst::USER_WRITE]]
+)]
 final class UserInputDto
 {
+    #[Groups([SelfcareConst::USER_WRITE])]
     #[Assert\NotBlank]
     #[ApiProperty(example: "john.doe")]
     public string $login;
 
+    #[Groups([SelfcareConst::USER_WRITE])]
     #[Assert\NotBlank]
     #[Assert\Email]
     #[ApiProperty(example: "john@example.com")]
     public string $email;
 
+    #[Groups([SelfcareConst::USER_WRITE])]
     #[Assert\NotBlank]
     #[ApiProperty(example: "0341234567")]
     public string $phone;
 
+    #[Groups([SelfcareConst::USER_WRITE])]
     #[Assert\Type('bool')]
     #[ApiProperty(example: true)]
     public bool $status;
 
+    #[Groups([SelfcareConst::USER_WRITE])]
     #[ApiProperty(example: "secret123")]
     public ?string $password = null;
 
+    #[Groups([SelfcareConst::USER_WRITE])]
     #[ApiProperty(example: false)]
     public ?bool $isLdapUser = false;
 
@@ -34,6 +49,7 @@ final class UserInputDto
      * 
      * @var list<array{id: int, rights: list<int>}>
      */
+    #[Groups([SelfcareConst::USER_WRITE])]
     #[Assert\Count(min: 1)]
     #[ApiProperty(example: [
         ['id' => 1, 'rights' => [2, 3]],
