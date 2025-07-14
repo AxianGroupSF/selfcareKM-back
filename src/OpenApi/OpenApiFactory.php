66c062886e82da4fcc8f487b8e5c7ad3aebb42f5
Francky Razakamanantsoa <francky.razakamanantsoa@pulse.mg>
2025-07-14 10:53:14 +0300

    Fleet Management : custom API summary for msisdnfleet endpoints

--- src/OpenApi/OpenApiFactory.php
+++ src/OpenApi/OpenApiFactory.php
@@ -25,6 +25,17 @@ class OpenApiFactory implements OpenApiFactoryInterface
             }
         }
 
+        $pathItem = $openApi->getPaths()->getPath('/api/msisdnfleet/{id}/bundles');
+        if ($pathItem) {
+            $operation = $pathItem->getPatch();
+            if ($operation instanceof Operation) {
+                $newOperation = $operation->withSummary('Updates bundles linked to a msisdnfleet')
+                    ->withDescription('This operation replaces all msisdnfleet bundles.');
+
+                $openApi->getPaths()->addPath('/api/msisdnfleet/{id}/bundles', $pathItem->withPatch($newOperation));
+            }
+        }
+
         return $openApi;
     }
 }
