e11a1b72a00850d018afc395e9e7d2cff84a45dd
Francky Razakamanantsoa <francky.razakamanantsoa@pulse.mg>
2025-07-14 11:38:25 +0300

    Fleet management : Const for Msisdn Fleet status

--- src/Constante/SelfcareConst.php
+++ src/Constante/SelfcareConst.php
@@ -3,15 +3,18 @@ namespace App\Constante;
 
 class SelfcareConst
 {
-    const INVALID_CREDENTIALS = 'Login ou mot de passe incorrect';
-    const USER_READ           = "user:read";
-    const USER_WRITE          = "user:write";
-    const USER_UPDATE         = "user:update";
-    const ROLE_READ           = "role:read";
-    const ROLE_WRITE          = "role:write";
-    const RIGHT_READ          = "right:read";
-    const USER_COMPANY_WRITE  = "user:company:write";
-    const BUNDLE_READ         = "bundle:read";
-    const BUNDLE_WRITE        = "bundle:write";
-    const MSISDN_FLEET_WRITE = "msisdn:fleet:write";
+    const INVALID_CREDENTIALS      = 'Login ou mot de passe incorrect';
+    const USER_READ                = "user:read";
+    const USER_WRITE               = "user:write";
+    const USER_UPDATE              = "user:update";
+    const ROLE_READ                = "role:read";
+    const ROLE_WRITE               = "role:write";
+    const RIGHT_READ               = "right:read";
+    const USER_COMPANY_WRITE       = "user:company:write";
+    const BUNDLE_READ              = "bundle:read";
+    const BUNDLE_WRITE             = "bundle:write";
+    const MSISDN_FLEET_WRITE       = "msisdn:fleet:write";
+    const MSISDN_STATUS_ENABLE     = "ENABLE";
+    const MSISDN_STATUS_DISABLE    = "DISABLE";
+    const MSISDN_STATUS_INPROGRESS = "INPROGRESS";
 }
