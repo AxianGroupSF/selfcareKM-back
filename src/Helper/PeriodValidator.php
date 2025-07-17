a9b6c5ae4c98c1db8601e1840df2e7370897366d
Francky Razakamanantsoa <francky.razakamanantsoa@pulse.mg>
2025-07-17 14:04:51 +0300

    Fleet Management : control enable/disable blocking check from .env

--- src/Helper/PeriodValidator.php
+++ src/Helper/PeriodValidator.php
@@ -1,10 +1,19 @@
 <?php
 namespace App\Helper;
 
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+
 class PeriodValidator
 {
+    public function __construct(private ParameterBagInterface $params)
+    {}
+
     public function isWithinAllowedPeriod(): bool
     {
+        if (! filter_var($this->params->get('activate_verif_period'), FILTER_VALIDATE_BOOLEAN)) {
+            return true;
+        }
+
         $today = (new \DateTimeImmutable('now'))->format('j'); // jour du mois (1-31)
         return $today >= 1 && $today <= 25;
     }
